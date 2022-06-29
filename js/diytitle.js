7fe8206a48ec4e04d9d8a5bc4fb5cb6472189b72
yml2213 <a2213290201@gmail.com>
2022-06-29 10:14:21 +0800

    Site updated: 2022-06-29 10:14:20

---
+++ js/diytitle.js
@@ -0,0 +1,18 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！';
+        clearTimeout(titleTime);
+    }
+    else {
+        //返回当前页面时标签显示内容
+        document.title = '♪(^∇^*)欢迎回来！' + OriginTitile;
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
